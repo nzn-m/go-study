ccff9adb59af8d8921a4eec57fd63d4569b04482
松浦 <>
2020-04-18 21:50:08 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo"
+)
+
+type User struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+func main() {
+	// サーバー用のインスタンスの取得
+	e := echo.New()
+	// ユーザー
+	u := User{
+		Email:    "me@example.com",
+		Password: "password",
+	}
+	// ルーティング設定
+	e.GET("/helloworld", helloWorld)
+	e.POST("/login", func(c echo.Context) error {
+		r := new(User)
+		if err := c.Bind(r); err != nil {
+			return c.String(http.StatusBadRequest, err.Error())
+		}
+
+		//debug
+		fmt.Printf("u.Email : %v\n", u.Email)
+		fmt.Printf("u.Password : %v\n", u.Password)
+		fmt.Printf("r.Email : %v\n", r.Email)
+		fmt.Printf("r.Password : %v\n", r.Password)
+
+		// もしうえのEmailとcurlでにゅうりょくしたEmailがいしないか
+		//それか,うえのパスワードとcurlで入力したpasswordが一致しなければ,"login fail"としゅつりょく
+		if r.Email != u.Email || r.Password != u.Password {
+			return c.String(http.StatusUnauthorized, "login fail")
+		}
+		// 暫定
+		token := "sampletoken"
+		name := "なずな"
+		return c.String(http.StatusOK, "{\"token\":\""+token+"\"},{\"name\":\""+name+"\"}")
+	})
+	// サーバー起動
+	e.Logger.Fatal(e.Start(":1324"))
+}
+func helloWorld(c echo.Context) error {
+	return c.String(http.StatusOK, "hello world!!")
+}
